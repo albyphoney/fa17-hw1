9c7c8f69d70e029219f4f5a42c7cf8ae98ee027c
Albert Phone <albertphone@berkeley.edu>
2017-09-19 23:15:44 -0700

    done

--- foobar.rb
+++ foobar.rb
@@ -1,8 +1,11 @@
 class Foobar
 
   def self.baz(a)
-    # Class method
-    # Call with `Foobar.baz`
+    a.map! {|x| x.to_i + 2}
+    a.delete_if &:odd?
+    a.uniq!
+    a.delete_if {|x| x >= 10}
+    a.inject(0){|sum,x| sum + x }
   end
 end
 
